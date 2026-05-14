a6d304f380cef7d983e87585517488cf7a5fc437
Mindsetmot <finteresting17@gmail.com>
2026-05-14 18:35:08 +0700

    fix path navigation to parent directory

--- auth/js/script.js
+++ auth/js/script.js
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     localStorage.setItem('account_id', res.data.account_id);
                     localStorage.setItem('user_role', res.data.role);
                     // Redirect ke halaman utama
-                    setTimeout(() => { window.location.href = '../index.html'; }, 1500);
+                    setTimeout(() => { window.location.href = '../'; }, 1500);
                 } else {
                     setTimeout(() => { switchPage('login'); }, 1500);
                 }
